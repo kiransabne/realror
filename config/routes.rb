034424c3cdb173cc4385e85c6ff1a6c2509d9402
Jorge Flores <jorge@ievolutioned.com>
2013-08-02 12:25:13 -0500

    routes modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Realror::Application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  devise_for :models
+  # devise_for :admins
+  # devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'landing#index'
+  devise_for :users, :path => "/", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+  devise_for :admins, :controllers => { :sessions => "admins/sessions", :registrations => "admins/registrations", :passwords => "admins/passwords" }
+  #devise_for :admins
 
-   #match '/dashboard' => 'home#dashboard', :as => 'user_root'
-  #devise_for :Users do
-  #     get "/login", :to => "devise/sessions#new" # Add a custom sign in route for user sign in
-  #     get "/logout", :to => "devise/sessions#destroy" # Add a custom sing out route for user sign out
-  #     get "/register", :to => "devise/registrations#new" # Add a Custom Route for Registrations
+  # devise_for :admins, :controllers => { :sessions => "admins/sessio ns" }
+
+  # devise_for :Admins do
+  #     get "admin/login", :to => "devise/sessions#new" # Add a custom sign in route for user sign in
+  #     get "admin/logout", :to => "devise/sessions#destroy" # Add a custom sing out route for user sign out
+  #     get "admin/register", :to => "devise/registrations#new" # Add a Custom Route for Registrations
   #  end
 
    #devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
