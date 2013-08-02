dcde379444f99d39d89b74176d9b2986a72709d5
Jorge Flores <jorge@ievolutioned.com>
2013-08-02 09:52:21 -0500

    devise sign up admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Realror::Application.routes.draw do
+  devise_for :admins
   devise_for :users
   devise_for :models
   # The priority is based upon order of creation: first created -> highest priority.
