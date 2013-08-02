ef531dfd697adcda0593a5fabb9a2be7a00a6fea
Jorge Flores <jorge@ievolutioned.com>
2013-08-01 23:51:12 -0500

    fist commit again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Realror::Application.config.secret_key_base = '4f2b0a4bb2f3d1b1f95364903c997f5e96d7ddc4c9be44df0d4c20437c09a84405a3bc6748d55428171f0e4b43bad9757146b4ed72cb732db8473d68cad479b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Realror::Application.config.session_store :cookie_store, key: '_realror_session'
