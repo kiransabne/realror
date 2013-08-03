4860d375e3eef62574e3ee08c0059ed6a77d6f75
Jorge Flores <jorge@ievolutioned.com>
2013-08-03 15:23:18 -0500

    CRUD categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,59 @@
+class CategoriesController < ApplicationController
+	before_filter :authenticate_admin!
+	before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+	layout "admin"
+	
+	  def index
+		@categories = Category.all
+	  end
+
+	  def show
+	  end
+
+	  def new
+	    @category = Category.new
+	  end
+
+	  def edit
+	  
+	  end
+
+	  def create
+	    @category = Category.new(category_params)
+
+	    respond_to do |format|
+	      if @category.save
+	        format.html { redirect_to categories_path, notice: 'category created.' }
+	      else
+	        format.html { render action: 'new' }
+	      end
+	    end
+	  end 
+
+	  def update
+	    respond_to do |format|
+	      if @category.update(category_params)
+	        format.html { redirect_to categories_path, notice: 'The category was successfully updated.' }
+	      else
+	        format.html { render action: 'edit' }
+	      end
+	    end
+	  end
+
+	  def destroy
+	    @category.destroy
+	    respond_to do |format|
+	      format.html { redirect_to categories_path }
+	    end
+	  end
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
+end
