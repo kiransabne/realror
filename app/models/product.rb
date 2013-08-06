8ce6e4f87e093005414d8d9fa01334485f892b67
Jorge Flores <jorge@ievolutioned.com>
2013-08-06 14:17:57 -0500

    basic test models on products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+	validates :name, :description, :price, :category_id, presence: true
+	#validates_associated :books
 	belongs_to :category
 	has_attached_file :photo, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>", :mini => "50x50>" }, :default_url => "missing_:style.png"
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def test_the_truth
+  	assert true
+  end
+
+  def test_validations
+  	product = Product.new
+  	assert !product.save, "should not save product without name, category, price or description" 
+  end
+
 end
