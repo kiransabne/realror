657a7d3ac4a92e69831cbd6b054b04483f5ed7e2
Jorge Flores <jorge@ievolutioned.com>
2013-08-08 11:43:11 -0500

    test sign up

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -2,3 +2,16 @@ require 'spec_helper'
 
 # Put your acceptance spec helpers inside spec/acceptance/support
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+def login_user!(email, password)
+  visit homepage
+  click_link_or_button 'Login to Admin'
+  click_link_or_button 'Sign up'
+
+  fill_in :admin_email,   :with => email 
+  fill_in :admin_password, :with => password
+  fill_in :admin_password_confirmation, :with => password
+
+  click_link_or_button 'Sign up'
+ 
+end
--- spec/acceptance/my_first_feature_spec.rb
+++ spec/acceptance/my_first_feature_spec.rb
@@ -18,4 +18,39 @@ feature 'My first feature', %q{
 		page.should have_link "Spanish"
    end
 
+=begin
+  background do
+    login_user!('negoxp87@gmail.com', 'Pasion8576')
+  end
+
+   scenario "Add products to store" do
+     visit new_product_path
+
+	 fill_in :product_name,   :with => 'producto 1' 
+  	 fill_in :product_description, :with => 'descripcion prduto'
+  	 fill_in :product_price,   :with => '32' 
+
+     #click_link_or_button 'Create Product'
+     click_button 'Create Product'
+
+     page.should have_css('div', :text => 'Product created.')
+   end
+=end
+
+scenario "login like a admin" do
+  visit homepage
+  click_link_or_button 'Login to Admin'
+  click_link_or_button 'Sign up'
+
+  fill_in :admin_email,   :with => 'negoxp@gmail.com' 
+  fill_in :admin_password, :with => 'Pasion8576'
+  fill_in :admin_password_confirmation, :with => 'Pasion8576'
+
+  click_link_or_button 'Sign up'
+  
+  page.should have_css('div', :text => 'Welcome! You have signed up successfully.')
+  
+end
+
+
 end
