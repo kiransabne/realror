8517364e4d2cc7f11040708c3e6948bfa9ecc2e2
Jorge Flores <jorge@ievolutioned.com>
2013-08-07 23:55:56 -0500

    fix main menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_locale
+  before_filter :set_locale, :get_menu
 def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
 end
@@ -13,6 +13,12 @@ protect_from_forgery
   def default_url_options(options={})
   logger.debug "default_url_options is passed options: #{options.inspect}\n"
   { :locale => I18n.locale }
-   end
+   end 
+
+  def get_menu
+    @categories = Category.roots
+    @products = Product.all.limit(4)
+  end
+
 
 end
